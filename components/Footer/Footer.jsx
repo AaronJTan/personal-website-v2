b24331cca22ad93a2608ef779b93cc9b5e00b50c
Aaron Tan <aaronjtan00@gmail.com>
2023-11-07 22:19:23 -0500

    feat: use date utility method to display current year for copyright

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { getCurrentYear } from "@/utils/dateHelper";
 import SocialMediaLinks from "../SocialMediaLinks/SocialMediaLinks";
 
 export default function Footer() {
@@ -5,7 +6,7 @@ export default function Footer() {
         <footer className="p-6 text-center">
             <p className="mb-1">Let's stay connected!</p>
             <SocialMediaLinks />
-            <p className="text-xs light-gray-text mt-1">Copyright ©2023 Aaron Tan. All Rights Reserved.</p>
+            <p className="text-xs light-gray-text mt-1">Copyright ©{getCurrentYear()} Aaron Tan. All Rights Reserved.</p>
         </footer>
     );
 }
