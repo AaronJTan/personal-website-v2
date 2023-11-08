55c6b2b708cea646a2ff740e6bdff0e7dde4db51
Aaron Tan <aaronjtan00@gmail.com>
2023-11-08 00:46:49 -0500

    feat: projects section

--- components/NavigationSections/Projects/Projects.js
+++ components/NavigationSections/Projects/Projects.js
@@ -1,8 +1,48 @@
+import TechStack from "@/components/TechStack/TechStack";
+import { socialMediaIcons } from "@/config/iconsConfig";
+import { projectsConfig } from "@/config/projectsConfig";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function Projects() {
     return (
-        <section>
-            Projects Page In Progress...
+        <section className="flex flex-col gap-8">
+            {projectsConfig.map((data, key) => (
+                <article key={key} className="grid grid-flow-col auto-cols-auto gap-2">
+                    <div>
+                        <h2 className="text-xl font-bold">{data.name}</h2>
+                        <TechStack techStack={data.techStack} />
+
+                        <div>
+                            {data.description.map((paragraph, index) => <p key={index}>{paragraph}</p>)}
+                        </div>
+
+                        <div className="flex gap-2 items-baseline">
+                            {
+                                data.githubLink && <a
+                                    className="icon-link"
+                                    href={data.githubLink}
+                                    title="GitHub"
+                                    target="_blank"
+                                >
+                                    <FontAwesomeIcon icon={socialMediaIcons["GitHub"]} size="2xl" />
+                                </a>
+                            }
+
+                            {
+                                data.link && <a
+                                    className="icon-link"
+                                    href={data.link}
+                                    title="Link"
+                                    target="_blank"
+                                >
+                                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xl" />
+                                </a>
+                            }
+                        </div>
+                    </div>
+                </article>
+            ))}
         </section>
     );
 }
---
+++ config/projectsConfig.js
@@ -0,0 +1,45 @@
+export const projectsConfig = [
+    {
+        name: "Daily Updates",
+        description: ["A web application that provides a convenient and centralized platform to access articles from various news websites and the latest deals."],
+        techStack: ["Next.js", "Tailwind CSS", "Spring Boot", "jsoup", "Docker"],
+        githubLink: "https://github.com/AaronJTan/DailyUpdates",
+        link: "https://dailyupdates.aarontan.ca",
+    },
+    {
+        name: "The Physio Spot Appointment Availability",
+        description: [
+            "A web application that streamlines the process of viewing available physiotherapy follow-up appointments at The Physio Spot Markham branch by eliminating the multi-page process of selecting a branch, service, and physiotherapist."
+        ],
+        techStack: ["Next.js", "Tailwind CSS"],
+        githubLink: "https://github.com/AaronJTan/DailyUpdates",
+        link: "https://physio-spot-availability.vercel.app/"
+    },
+    {
+        name: "Crazy Picasso",
+        description: ["A web-based multiplayer drawing and guessing game."],
+        techStack: ["React.js", "Material UI", "Express.js", "Node.js", "Socket.IO", "MongoDB", "Docker", "Nginx"],
+        githubLink: "https://github.com/AaronJTan/crazy-picasso",
+    },
+    {
+        name: "Zoomer",
+        description: ["A microservices-based REST API empowering users to seamlessly request rides from available drivers within their proximity."],
+        techStack: ["Java", "Docker", "PostgreSQL", "MongoDB", "Neo4j"],
+    },
+    {
+        name: "African Impact Challenge Community and E-learning",
+        description: ["Designed for young entrepreneurs participating in the African Impact Challenge, this online platform facilitates both online learning and serves as a forum for entrepreneurs to engage with fellow participants and connect with potential partners like investors, service providers, and corporations."],
+        techStack: ["React.js", "Material UI", "Express.js", "Node.js", "PostgreSQL"],
+        githubLink: "https://github.com/AaronJTan/African-Impact-Challenge-Community-E-learning",
+    },
+    {
+        name: "Mindfulness",
+        description: ["A web application featuring a dynamic display of motivational quotes, promoting inspiration and personal growth."],
+        techStack: ["React.js", "Material UI", "Spring Boot", "MySQL"],
+    },
+    {
+        name: "Handwritten Digit Recognition",
+        description: ["A program that uses the k-nearest-neighbour (kNN) machine learning algorithm to classify handwritten digits using parallel programming with forks and pipes."],
+        techStack: ["C"],
+    },
+]
